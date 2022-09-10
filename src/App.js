4d26143cbc75c34fa1b46fdc93e2ad2d2deb5d0e
Andrew Walters <andrewjpwalters@gmail.com>
2022-09-09 22:45:55 -0500

    add inital logic and fetch requests to App and NewMovie

--- src/App.js
+++ src/App.js
@@ -1,13 +1,50 @@
-import React from "react";
+import React, { useEffect } from "react";
 import NewMovie from "./NewMovie";
 import MovieList from "./MovieList";
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:9292/movies")
+      .then((r) => r.json())
+      .then((movies) => setMovies(movies))
+  }, []);
+
+  function handleAddMovie(newMovie) {
+    setMovies([...movies, newMovie]);
+  };
+
+  function handleDeleteMovie(id) {
+    const updatedMovies = movies.filter((movie) => movie.id !== id);
+    setMovies(updatedMovies)
+  };
+
+  function handleUpdateMovie(updatedMovieObj) {
+    const updatedMovies = movies.map((movie) => {
+      if (movie.id === updatedMovieObj.id) {
+        return updatedMovieObj;
+      } else {
+        return movie;
+      }
+    })
+    setMovies(updatedMovies)
+  };
+
+  const displayedMovies = movies.filter((movie) =>
+    movie.name.toLowerCase().includes(search.toLowerCase())
+  )
+
   return (
     <div>
       <h1> Hello from App!</h1>
-      <NewMovie />
-      <MovieList />
+      <NewMovie onAddMovie={handleAddMovie} />
+      <MovieList
+        movies={displayedMovies}
+        onMessageDelete={handleDeleteMovie}
+        onUpdateMovie={handleUpdateMovie}
+      />
     </div>
   );
 }
--- src/NewMovie.js
+++ src/NewMovie.js
@@ -1,8 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
 
 function NewMovie() {
+
+    const [formData, setFormData] = useState({
+        name: "",
+        genre: "",
+        year: "",
+        comment: ""
+    })
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        fetch("http://localhost:9292/movies", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(formData)
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log("Success", data);
+            })
+            .catch((error) => {
+                console.error("Error", error);
+            })
+    }
+
+    function handleChange(event) {
+        const key = event.target.id
+        const value = event.target.value
+        setFormData({
+            ...formData,
+            [key]: value
+        })
+    }
+
     return (
-        <h1>Hello from New Movie!</h1>
+        <>
+            <h1>Hello from New Movie!</h1>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    id="name"
+                    value={formData.name}
+                    onChange={handleChange}
+                    placeholder="title"
+                />
+                <select
+                    id="genre"
+                    value={formData.genre}
+                    onChange={handleChange}
+                >
+                    {/* {genreData} */}
+                </select>
+                <input
+                    type="text"
+                    id="year"
+                    value={formData.year}
+                    onChange={handleChange}
+                    placeholder="year"
+                />
+                <input
+                    type="text"
+                    id="comment"
+                    value={formData.comment}
+                    onChange={handleChange}
+                    placeholder="comment"
+                />
+                <input type="submit" value="Submit" />
+            </form>
+        </>
     )
 }
 
